d4d46dd1edf588719410bb6f635e6b44203074f8
jaejae200 <kj31049@naver.com>
2022-11-16 13:54:13 +0900

    feat: 회원가입 기능

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
 import requests
 
 # Create your views here.
 
 def signup(request):
+    if request.method == 'POST':
+        sigup_form = CustomUserCreationForm(request.POST)
+        if sigup_form.is_valid():
+            user = sigup_form.save()
+            login(request, user)
+    else:
+        sigup_form = CustomUserCreationForm()
+        
     return render(request, 'accounts/signup.html')
--- mypjt/settings.py
+++ mypjt/settings.py
@@ -126,3 +126,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# User Model
+
+AUTH_USER_MODEL = 'accounts.User'
